d478ab2b064c8b918b59bcf0ffceee8ac5ec09d1
UkCastle <jo631@naver.com>
2021-10-04 15:42:48 +0000

    fix: 정상실행...

--- train/__main__.py
+++ train/__main__.py
@@ -9,9 +9,6 @@ import os
 
 def main():
 
-	# Set venv
-	# os.system("source /opt/conda/bin/activate")
-
 	# Call Initialization File 
 	customDict = makeDictByConfig()
 
@@ -23,7 +20,16 @@ def main():
 		datasetName = customDict.name.train_dataset,
 		jsonPath = customDict.path.train_json,
 		imageDirPath = customDict.path.image_root,
-		numOfClasses = customDict.general.classes
+		numOfClasses = customDict.general.classes,
+		istrain=True
+	)
+
+	registerDataset(
+		datasetName = customDict.name.test_dataset,
+		jsonPath = customDict.path.test_json,
+		imageDirPath = customDict.path.image_root,
+		numOfClasses = customDict.general.classes,
+		istrain=False
 	)
 
 	# Set Detectron Config by Arg
@@ -31,7 +37,8 @@ def main():
 
 	# Set Mapper
 	# TODO: 문자열로 되어있는 부분 바꾸기
-	mapperModule = getattr(import_module("train.mapper.base_mapper"), "BaseMapper")
+	# mapperModule = getattr(import_module("train.mapper.base_mapper"), "BaseMapper")
+	mapperModule = getattr(import_module("train.mapper.custom_mapper"), "CustomMapper")
 	mapper = mapperModule()
 	
 	# Set Trainer
@@ -40,9 +47,12 @@ def main():
 	trainerModule.outputEvalDir = customDict.path.output_eval_dir
 	trainerModule.mapper = mapper
 	
+	print(trainerModule.outputEvalDir)
+
 	# Start Train
 	# TODO: 세분화하기
 	os.makedirs(customDict.path.output_dir)
+	# os.makedirs(trainerModule.outputEvalDir)
 	trainer = trainerModule(cfg = parser.getConfig())
 	trainer.resume_or_load(resume=customDict.option.resume)
 	trainer.train()
