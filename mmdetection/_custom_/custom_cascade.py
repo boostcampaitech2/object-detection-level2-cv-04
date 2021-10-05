f6e435fe70c37c9f28b1c50df1298370a8b64d44
zzin <8639sung@gmail.com>
2021-10-05 14:38:01 +0000

    feat: Add custom yolo

--- mmdetection/_custom_/custom_cascade.py
+++ mmdetection/_custom_/custom_cascade.py
@@ -1,5 +1,5 @@
 _base_ = [
-    '../_custom_/retinanet_r50_fpn.py',
+    '../_custom_/cascade_rcnn_r50_fpn.py',
     '../_custom_/dataset.py',
     '../_custom_/default_runtime.py',
     '../_custom_/schedule.py',
@@ -8,7 +8,6 @@ _base_ = [
 model = dict(
     backbone=dict(
         norm_cfg=dict(requires_grad=False),
-        norm_eval=True,
         style='caffe',
         init_cfg=dict(
             type='Pretrained',
@@ -17,13 +16,13 @@ img_norm_cfg = dict(
     mean=[103.530, 116.280, 123.675], std=[1.0, 1.0, 1.0], to_rgb=False)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
-    dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
+    dict(type='LoadAnnotations', with_bbox=True),
     dict(type='Resize', img_scale=(1024, 1024), keep_ratio=True),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
     dict(type='DefaultFormatBundle'),
-    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks']),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
 ]
 test_pipeline = [
     dict(type='LoadImageFromFile'),
--- mmdetection/_custom_/custom_retinanet.py
+++ mmdetection/_custom_/custom_retinanet.py
@@ -13,7 +13,6 @@ model = dict(
         init_cfg=dict(
             type='Pretrained',
             checkpoint='open-mmlab://detectron2/resnet50_caffe')))
-# use caffe img_norm
 img_norm_cfg = dict(
     mean=[103.530, 116.280, 123.675], std=[1.0, 1.0, 1.0], to_rgb=False)
 train_pipeline = [
