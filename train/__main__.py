a44baed55c7423fa052fef6bb2265afa2afb3a6d
UkCastle <jo631@naver.com>
2021-10-05 06:30:39 +0000

    feat(TODO): 인자로 클래스명 받게 바꿈

--- train/__main__.py
+++ train/__main__.py
@@ -2,7 +2,7 @@ from train.src.config_arg_parser import ConfigArgParser
 from train.src.set_seed import setSeed
 from train.src.register_dataset import registerDataset
 from train.src.module_caller import getModule
-from config.load_config import makeDictByConfig
+from config.save_config import saveConfig
 
 from importlib import import_module
 
@@ -10,8 +10,9 @@ import os
 
 def main():
 
-	# Call Initialization File 
-	customDict = makeDictByConfig()
+	# Set Detectron Config by Arg
+	parser = ConfigArgParser()
+	customDict = parser.getCustomDict()
 
 	# Set Seed
 	setSeed(customDict.hyperparam.seed)
@@ -33,32 +34,24 @@ def main():
 		istrain=False
 	)
 
-	# Set Detectron Config by Arg
-	parser = ConfigArgParser(customDict)
 
 	# Set Mapper
-	# TODO: 문자열로 되어있는 부분 바꾸기
-	# mapperModule = getattr(import_module("train.mapper.base_mapper"), "BaseMapper")
-	mapperModule = getModule("mapper","custom_mapper")
+	mapperModule = getModule("mapper",customDict.name.mapper)
 	mapper = mapperModule()
 	
 	# Set Sampler
-	samplerModule = getModule("sampler","custom_sampler")
+	samplerModule = getModule("sampler",customDict.name.sampler)
 	sampler = samplerModule(seed=customDict.hyperparam.seed)
 
 	# Set Trainer
-	# TODO: 문자열로 되어있는 부분 바꾸기
-	trainerModule = getModule("trainer","base_trainer")
+	trainerModule = getModule("trainer",customDict.name.trainer)
 	trainerModule.outputEvalDir = customDict.path.output_eval_dir
 	trainerModule.mapper = mapper
 	trainerModule.sampler = sampler
-	
-	print(trainerModule.outputEvalDir)
 
 	# Start Train
-	# TODO: 세분화하기
 	os.makedirs(customDict.path.output_dir)
-	# os.makedirs(trainerModule.outputEvalDir)
+	saveConfig(customDict.path.output_dir, customDict)
 	trainer = trainerModule(cfg = parser.getConfig())
 	trainer.resume_or_load(resume=customDict.option.resume)
 	trainer.train()
