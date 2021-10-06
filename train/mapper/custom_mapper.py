0137b5bb3f00e67822888a57a2c741bfab3fa7e3
UkCastle <jo631@naver.com>
2021-10-06 16:43:31 +0000

    chore: custom mapper

--- train/mapper/custom_mapper.py
+++ train/mapper/custom_mapper.py
@@ -6,5 +6,8 @@ class CustomMapper(BaseMapper):
 	def __init__(self):
 			super().__init__()
 			self._addTransForm(T.RandomFlip(prob=0.5, horizontal=False, vertical=True))
-			self._addTransForm(T.RandomBrightness(0.8, 1.8))
-			self._addTransForm(T.RandomContrast(0.6, 1.3))
+			self._addTransForm(T.RandomFlip(prob=0.5, horizontal=True, vertical=False))
+			self._addTransForm(T.RandomBrightness(0.8,1.8))
+			self._addTransForm(T.RandomContrast(0.6,1.3))
+			self._addTransForm(T.RandomCrop("absolute",(640,640)))
+
