22f6b8a39cd8894619cd79fed45ca54af80d2161
seung-sss <sjun804@gmail.com>
2021-10-14 20:11:20 +0000

    change directory and modify base directory code

---
+++ mmdetection/custom/2_faster_rcnn/0_1_fasterrcnn+adamw.py
@@ -0,0 +1,6 @@
+_base_ = [
+    '../model/faster_rcnn_r50_fpn_trash.py',
+    '../dataset/dataset_basic.py',
+    '../default_runtime.py',
+    '../schedule/schedule.py'
+]
---
+++ mmdetection/custom/3_cascade_rcnn/1_cascade+adamw.py
@@ -0,0 +1,7 @@
+# config 파일들이 각가 떨어져 있기 때문에 합쳐주는 과정 필요!
+_base_ = [
+    '../model/cascade_rcnn_r50_fpn_trash.py',
+    '../dataset/dataset_basic.py',
+    '../default_runtime.py',
+    '../schedule/schedule.py'
+]
