2178741ca575cc2de3d4144eb43eca86547ca813
seung-sss <sjun804@gmail.com>
2021-10-14 19:12:16 +0000

    add cascade+adamw+swinS+aug model test

---
+++ mmdetection/custom/4_cascade+adamw+swinS+aug.py
@@ -0,0 +1,30 @@
+# batch 8에서 cuda out of memory 오류 발생 (4로 낮추자!)
+_base_ = [
+    'model/cascade_rcnn_r50_fpn_trash.py',
+    'dataset_albu.py',
+    'default_runtime.py',
+    'schedule.py'
+]
+
+# swin-S
+pretrained = 'https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_small_patch4_window7_224.pth'
+model = dict(
+    backbone=dict(
+        _delete_=True,
+        type='SwinTransformer',
+        embed_dims=96,
+        depths=[2, 2, 18, 2], # 변경
+        num_heads=[3, 6, 12, 24],
+        window_size=7,
+        mlp_ratio=4,
+        qkv_bias=True,
+        qk_scale=None,
+        drop_rate=0.,
+        attn_drop_rate=0.,
+        drop_path_rate=0.2,
+        patch_norm=True,
+        out_indices=(0, 1, 2, 3),
+        with_cp=False,
+        convert_weights=True,
+        init_cfg=dict(type='Pretrained', checkpoint=pretrained)),
+    neck=dict(in_channels=[96, 192, 384, 768]))
